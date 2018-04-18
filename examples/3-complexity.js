e967b7cea59cfb0739808f70201d34ffd22060be
Jack Hsu <jack.hsu@gmail.com>
2018-04-18 13:31:16 -0400

    adds complexity example

---
+++ examples/3-complexity.js
@@ -0,0 +1,33 @@
+/*
+ * Here's an example of checking if user-agent is a bot.
+ *
+ * There are two versions, isBotA (which is complex), isBotB (which is simpler).
+ */
+
+function isBotA(userAgent: string) {
+  // Ignore all the user agents we don't care about
+  if (userAgent.indexOf('Pulsepoint') !== -1) return true
+  if (userAgent.indexOf('CloudFlare') !== -1) return true
+  if (userAgent.indexOf('Phantom') !== -1) return true
+  if (userAgent.indexOf('Android 4') !== -1) return true
+  if (userAgent.indexOf('Android 5') !== -1) return true
+  if (userAgent.indexOf('Googlebot') !== -1) return true
+  if (userAgent.indexOf('AdsBot-Google') !== -1) return true
+  if (userAgent.indexOf('Linux') !== -1) return true
+  if (userAgent.indexOf('Chrome/3') !== -1) return true
+  if (userAgent.indexOf('Bingbot') !== -1) return true
+  if (userAgent.indexOf('facebookexternalhit') !== -1) return true
+  if (userAgent.indexOf('facebot') !== -1) return true
+  if (userAgent.indexOf('python') !== -1) return true
+  if (userAgent.indexOf('ia_archiver') !== -1) return true
+  return false
+}
+
+const BOTS = [
+  'Pulsepoint', 'CloudFlare', 'Phantom', 'Android 4', 'Android 5', 'Googlebot', 'AdsBot-',
+  'Linux', 'Chrome/', 'Bingbot', 'facebookexternalhit', 'facebot', 'python', 'ia_archiver'
+]
+
+function isBotB(userAgent: string) {
+  return BOTS.includes(userAgent)
+}
